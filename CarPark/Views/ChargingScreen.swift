126adcd2544d57b29fae69f2331e53e87c2302d0
Tsani Chico Bragi <tsanichico35@gmail.com>
2024-04-19 10:40:33 +0700

    update main view

---
+++ CarPark/Views/ChargingScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ChargingScreen.swift
+//  CarPark
+//
+//  Created by Tsani Chico Bragi on 03/04/24.
+//
+
+import SwiftUI
+
+struct ChargingScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChargingScreen()
+}
