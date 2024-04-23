30da6ba1d48fc59279a4379d64ac8b1911272396
Tsani Chico Bragi <tsanichico35@gmail.com>
2024-04-23 22:34:06 +0700

    update view chargingports

---
+++ CarPark/Views/ChargingPorts.swift
@@ -0,0 +1,18 @@
+//
+//  ChargingPorts.swift
+//  CarPark
+//
+//  Created by Tsani Chico Bragi on 23/04/24.
+//
+
+import SwiftUI
+
+struct ChargingPorts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChargingPorts()
+}
