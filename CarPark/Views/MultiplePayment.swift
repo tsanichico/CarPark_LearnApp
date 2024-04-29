1596968944ede535ab3ce222f96b1efb52f608f5
Tsani Chico Bragi <tsanichico35@gmail.com>
2024-04-29 20:01:27 +0700

    create multiplepayment view

---
+++ CarPark/Views/MultiplePayment.swift
@@ -0,0 +1,18 @@
+//
+//  MultiplePayment.swift
+//  CarPark
+//
+//  Created by Tsani Chico Bragi on 27/04/24.
+//
+
+import SwiftUI
+
+struct MultiplePayment: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultiplePayment()
+}
