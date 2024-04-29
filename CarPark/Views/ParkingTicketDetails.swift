affbaef6303e7656c7123b78b393d9145d8e965e
Tsani Chico Bragi <tsanichico35@gmail.com>
2024-04-29 20:04:10 +0700

    update minor multiplepayment view

---
+++ CarPark/Views/ParkingTicketDetails.swift
@@ -0,0 +1,18 @@
+//
+//  ParkingTicketDetails.swift
+//  CarPark
+//
+//  Created by Tsani Chico Bragi on 29/04/24.
+//
+
+import SwiftUI
+
+struct ParkingTicketDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParkingTicketDetails()
+}
