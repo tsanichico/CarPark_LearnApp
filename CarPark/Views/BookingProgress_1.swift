234ec19dbec7f1de3b4e0c41ba780656e8a5cd2b
Tsani Chico Bragi <tsanichico35@gmail.com>
2024-05-02 18:48:01 +0700

    added new view bookingprogress

---
+++ CarPark/Views/BookingProgress_1.swift
@@ -0,0 +1,18 @@
+//
+//  BookingProgress_1.swift
+//  CarPark
+//
+//  Created by Tsani Chico Bragi on 02/05/24.
+//
+
+import SwiftUI
+
+struct BookingProgress_1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookingProgress_1()
+}
